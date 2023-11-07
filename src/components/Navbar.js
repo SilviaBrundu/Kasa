7af3002261616d15af9f87caf93a66de59c5113e
silvia brundu <silviabrundu13@gmail.com>
2023-11-07 23:09:00 +0100

    feat: add logo and sass files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom'
+import logo from '../img/LOGO.png'
  
 function Navbar() {
     return (
-        <nav>
-            <Link to='/'>Accueil</Link>
-            <Link to='/About'>A propos</Link>
-        </nav>
+        <div className='navbarContainer'>
+            <div className='logoContainer'>
+                <img className='logo' src={logo} alt="Kasa"  />    
+            </div>
+            <nav>
+                <Link to='/'>Accueil</Link>
+                <Link to='/About'>A propos</Link>
+            </nav>
+        </div>
     )
 }
 
