cbd2be6406f83800adbeff27075269576c87be1f
silvia brundu <silviabrundu13@gmail.com>
2023-11-07 21:04:38 +0100

    feat: add link and test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
     </Routes>
   </BrowserRouter>
+  // path contient l’URL dans notre navigateur qui dirigera vers le composant ;
+  // element va permettre de sélectionner le composant à afficher. 
+  // Les composants BrowserRouter, Routes et Route permettent de gérer l'affichage des différentes pages.
   );
 }
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+ 
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/About">A propos</Link>
+        </nav>
+    )
+}
+
+export default Navbar
