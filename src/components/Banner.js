a39f1bb08221aff93f321d5720342f8fafe51392
silvia brundu <silviabrundu13@gmail.com>
2023-11-08 22:05:09 +0100

    feat: add banner components and style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,13 +3,12 @@ import imageSource1 from "../img/ImageSource1.png"
 function Banner() {
     return (
         <div className='bannerContainer'>
-            <div className="bannerOpacity">
+            <div className="bannerOpacity"></div>
             <div className="bannerImgContainer">
                 <img className="bannerImg" src={imageSource1} alt="mer" />
             </div>
             <div className='bannerTitleContainer'>
                 <h1 className='bannerTitle'>Chez vous, partout et ailleurs</h1>
-            </div>
             </div>
       </div>
     )
--- src/components/BannerAbout.js
+++ src/components/BannerAbout.js
@@ -3,10 +3,9 @@ import imageSource2 from '../img/ImageSource2.png'
 function BannerAbout() {
     return (
         <div className='bannerContainer'>
-            <div className='bannerOpacity'>
-                <div className='bannerImgContainer'>
-                    <img className='bannerImg' src={imageSource2} alt="foret" />
-                </div>
+            <div className='bannerOpacity'></div>
+            <div className='bannerImgContainer'>
+                <img className='bannerImg' src={imageSource2} alt="foret" />
             </div>
         </div>
     )
