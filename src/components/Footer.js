f1e49b1ca99939db753e490518684b20cb0a34c4
silvia brundu <silviabrundu13@gmail.com>
2023-11-13 21:26:01 +0100

    feat: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import logo from '../img/LOGOF.png';
+
 function Footer() {
-    
-}
+
+  return (
+    <footer className='footer_container'>
+        <div className='footer_logo_container'>
+            <img className="footer_logo" src={logo} alt="Kasa"  />
+            <p className="footer_copyright">&#169;  2020 Kasa.All rights reserved</p>
+        </div>
+    </footer>
+  );
+};
 
 export default Footer
