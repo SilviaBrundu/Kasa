869eb8976b236a5595fdabb205d0ae0933a09297
silvia brundu <silviabrundu13@gmail.com>
2023-11-16 12:08:40 +0100

    fix: collapse arrow

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import Arrow from '../img/Arrow.png';
 
 function Collapse ({title, text}) {
-    const [ visible , setVisible ] = useState (false) 
+    const [ visible, setVisible ] = useState(false)
+    
+    const handleToggle = () => {
+        setVisible(!visible);
+      };
     
     return (
         <div className='about_container'>
-            <div className='about_content'>
+            <div onClick={handleToggle} className={`about_content ${visible ? 'rotateArrow' : ''}`}>
                 <h1 className='about_title'>{title}</h1>
-                <button className={`about_button_container ${visible? 'about_button_container_rotateDown' : ''}`} 
-                onClick={() => {setVisible(!visible)}}>
-                    <i className={`fa-solid ${visible? 'fa-chevron-down fa-2xl' : 'fa-chevron-up fa-2xl'}`}></i>
-                </button>
+                <img src={Arrow} alt="arrow" className="arrow"></img>
             </div>
             {visible&&(
                 <div className='about_text_content'>
@@ -21,64 +23,5 @@ function Collapse ({title, text}) {
     )   
 }
 
-// function Collapse ({title, text}) {
-//     const [ visible , setVisible ] = useState (false)
 
-//     return (
-//         <div className='about_container'>
-//             <div className='about_content'>
-//                 <h1 className='about_title'>{title}</h1>
-//                 <button className={`about_button_container ${visible? 'about_button_container_rotateUp' : 'about_button_container_rotateDown'}`} onClick={() => {setVisible(!visible)}}>
-//                         <i className={`fa-solid ${visible? 'fa-chevron-down fa-2xl' : 'fa-chevron-up fa-2xl'}`}></i>
-//                 </button>
-//             </div>
-//             {visible&&(
-//                 <div className='about_text_content'>
-//                     <p className='about_text'>{text}</p>
-//                 </div>
-//             )}
-//         </div>
-//     ) }
-
-//     return (
-//         <div className='about_container'>
-//             {/* <button className='about_button_container' onClick={() => setVisible(!visible)}> */}
-//                 <div className='about_content'>
-//                     <h1 className='about_title'>{title}</h1>
-//                     <button className='about_button_container' onClick={() => setVisible(!visible)}>
-//                         <i className={`fa-solid ${visible? 'fa-chevron-down fa-2xl' : 'fa-chevron-up fa-2xl'}`}></i>
-//                     </button>
-//                 </div>
-//             {/* </button> */}
-//             {visible&&(
-//                 <div className='about_text_content'>
-//                     <p className='about_text'>{text}</p>
-//                 </div>
-//             )}
-//         </div>
-//     )   
-// }
-
-// const [ visible , setVisible ] = useState (false);
-
-
-//   const toggle = () => {
-//     setVisible(!visible);
-//   };
-
-
-//   return (
-//     <div className='about_container'>
-//       <div className='about_content'>
-//         <h1 className='about_title'>{title}</h1>
-//         <button className='about_button_container' onClick={toggle}>
-//             <i  className={`fa-solid fa-chevron-up ${visible ? "rotate" : ""}`}></i>
-//         </button>
-//         </div>
-//       <div className={`about_text_content ${visible ? "dropDow" : ""}`}>
-//       <p className='about_text'>{text}</p>
-//       </div>
-//     </div>
-//   );
-// }
 export default Collapse;
