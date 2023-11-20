7eacdcf4c172e8748a095d6f3d6e332a415092cf
silvia brundu <silviabrundu13@gmail.com>
2023-11-20 20:13:11 +0100

    feat : offerCard file

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,4 +1,5 @@
-import Data from '../data/data.json'
+import Data from '../data/data.json';
+import OfferCard from './OfferCard';
 //je récupère le fichier jSon
 
 function Offers () {
@@ -6,16 +7,8 @@ function Offers () {
         <div className='cards_container_box'>
             <div className='cards_container'>{Data.map((offer, index) => { //index = key, aide react a identifier les elements qui vont changer
                 return(    
-                    <div key={index}> 
-                        <div className='card_content'> 
-                            <div className='card-content-img'>
-                                <img className= 'card_img' src={offer.cover} alt={offer.title}></img>
-                                <div className='card_opacity'></div>
-                            </div>
-                            <div className='card_content_title'>
-                                <h2 className='card_title'>{offer.title}</h2>
-                            </div>
-                        </div>
+                    <div key={index} >
+                        <OfferCard offer={offer} ></OfferCard>
                     </div>
                     )}
                 )}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ImageSource1 from "../img/ImageSource1.png"
 import Banner from '../components/Banner';
 import Offers from '../components/Offers';
-// import OfferCard from '../components/OfferCard';
 import Layout from '../components/Layout';
 
 function Home () {
     return (
         <Layout>
-                <Banner title={'Chez vous, partout et ailleurs'} image={ImageSource1} alt={'Vue sur la mer'}/>
-                <Offers />
-                {/* <OfferCard /> */}
+            <Banner title={'Chez vous, partout et ailleurs'} image={ImageSource1} alt={'Vue sur la mer'}/>
+            <Offers />    
         </Layout>
     )
 }
