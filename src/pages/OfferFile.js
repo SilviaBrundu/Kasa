de4477741409939a7d903be45b34b81f2978ca19
silvia brundu <silviabrundu13@gmail.com>
2023-11-21 10:03:46 +0100

    feat: layout on offerfile

--- src/pages/OfferFile.js
+++ src/pages/OfferFile.js
@@ -1,12 +1,19 @@
 import { useParams, Navigate } from 'react-router-dom';
 import Data from '../data/data.json';
+import Layout from '../components/Layout';
 
 function OfferFile() {
+    
     const { id } = useParams();
     const offerId = Data.find((element) => element.id === id);
 
     if (!offerId) {
         return <Navigate to="/*" replace />;
     }
+    return(
+        <Layout>
+
+        </Layout>
+    )
 }
     export default OfferFile
