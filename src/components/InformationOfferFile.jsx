ffaaacdf3e122a7b6e29869620d57d6ad3dfdc5f
silvia brundu <silviabrundu13@gmail.com>
2023-11-26 10:30:32 +0100

    fix: syntaxe

--- src/components/InformationOfferFile.jsx
+++ src/components/InformationOfferFile.jsx
@@ -6,8 +6,10 @@ import Data from '../data/data.json';
 function Information() {
     const { id } = useParams();
 
-    const title = Data.find((title) => title.id === id).title;
-    const location = Data.find((location) => location.id === id).location;
+    const title = Data.find((titleData) => titleData.id === id).title;
+    const location = Data.find((locationData) => locationData.id === id).location;
+    // const hostPicture = Data.find((hostPictureData) => hostPictureData.id === id).host.picture;
+    // const hostName = Data.find((hostNameData) => hostNameData.id === id).host.name;
 
     return (
         <div className='information_container'>
@@ -15,6 +17,11 @@ function Information() {
                 <h2 className='information_title'>{title}</h2>
                 <p className='information_location'>{location}</p>
             </div>
+            {/* <div className='information_host_content'>
+                <img className='informaton_host_picture' alt={hostName}>{hostPicture}</img>
+                <p className='information_host_name'>{hostName}</p>
+            </div> */}
+            
         </div>
     )
 }
