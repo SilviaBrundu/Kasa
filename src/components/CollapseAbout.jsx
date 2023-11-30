8b65a572b0df52e27fa7c0e7ad0ca49c72c0b356
silvia brundu <silviabrundu13@gmail.com>
2023-11-30 22:55:53 +0100

    feat: about data file and fix css collapse

--- src/components/CollapseAbout.jsx
+++ src/components/CollapseAbout.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Collapse from './Collapse';
+import aboutData from '../data/aboutData.json';
 
+function CollapseAbout () {
 
-function CollapseAbout (text) {
   return (
-    <div className='about_container'>
-      <Collapse title='Fiabilité' text='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos equipes.'/>
-      
-      <Collapse title='Respect' text='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.'/>
-
-      <Collapse title='Service' text='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.'/>
-
-      <Collapse title='Sécurité' text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de securité établis par nos services. En laissant une note aussi bien a l'hôte qu'au locataire, cela permet à nos equipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
-    </div>
-  )
+      <div className="about-container">
+        {aboutData.map((aboutData, index) => {
+          return (
+            <Collapse key={index} title={aboutData.title} text={<p className='about_text'>{aboutData.text}</p>}/>
+          );
+        })}
+      </div>
+  );
 }
 
-export default CollapseAbout;
+export default CollapseAbout
--- src/components/CollapseInfo.jsx
+++ src/components/CollapseInfo.jsx
@@ -10,10 +10,10 @@ function CollapseInfo ({equipments, description}) {
     return (
         <div className='info_collapse_container'>
             <div className='info_description_content'>
-                <Collapse title='Description' text={description}/>
+                <Collapse title='Description' text={<p>{description}</p>}/>
             </div>
             <div className='info_equipments_content'>
-                <Collapse title='Equipement' text={listEquipments}/>
+                <Collapse title='Equipement' text={<ul>{listEquipments}</ul>}/>
             </div>
         </div>
     )   
